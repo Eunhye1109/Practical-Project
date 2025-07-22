81cce0bdfcd2283bfa6193d4e614d6d38c11ba2f
Kimhyerim <ax1021@naver.com>
2025-07-22 10:49:10 +0900

    [Fix] 텍스트 스타일 수정

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,70 +1,17 @@
-import styled from '@emotion/styled'
 import React from 'react'
-import { title1Bold, title1Regular, title2Bold, title2Regular, body1SemiBold, body1Regular, body2SemiBold, body2Regular, caption1Light, caption2Light, caption1Regular, caption2Regular } from 'styles/typoStyle'
+import styled from '@emotion/styled'
+import { typoStyle } from 'styles/typoStyle';
 
 const TSt01 = styled.p`
-  ${({theme}) => title1Bold(theme)}
+  ${({theme}) => typoStyle.subTitle.bold(theme)}
 `;
 
-const TSt02 = styled.p`
-  ${({theme}) => title1Regular(theme)}
-`;
 
-const TSt03 = styled.p`
-  ${({theme}) => title2Bold(theme)}
-`;
-
-const TSt04 = styled.p`
-  ${({theme}) => title2Regular(theme)}
-`;
-
-const TSt05 = styled.p`
-  ${({theme}) => body1SemiBold(theme)}
-`;
-
-const TSt06 = styled.p`
-  ${({theme}) => body1Regular(theme)}
-`;
-
-const TSt07 = styled.p`
-  ${({theme}) => body2SemiBold(theme)}
-`;
-
-const TSt08 = styled.p`
-  ${({theme}) => body2Regular(theme)}
-`;
-
-const TSt09 = styled.p`
-  ${({theme}) => caption1Regular(theme)}
-`;
-
-const TSt10 = styled.p`
-  ${({theme}) => caption1Light(theme)}
-`;
-
-const TSt11 = styled.p`
-  ${({theme}) => caption2Regular(theme)}
-`;
-
-const TSt12 = styled.p`
-  ${({theme}) => caption2Light(theme)}
-`;
 
 const Home = () => {
   return (
     <div>
       <TSt01>title1Bold</TSt01>
-      <TSt02>title1Regular</TSt02>
-      <TSt03>title2Bold</TSt03>
-      <TSt04>title2Regular</TSt04>
-      <TSt05>body1SemiBold</TSt05>
-      <TSt06>body1Regular</TSt06>
-      <TSt07>body2SemiBold</TSt07>
-      <TSt08>body2Regular</TSt08>
-      <TSt09>caption1Regular</TSt09>
-      <TSt10>caption1Light</TSt10>
-      <TSt11>caption2Regular</TSt11>
-      <TSt12>caption2Light</TSt12>
     </div>
   )
 }
--- front/src/styles/typoStyle.ts
+++ front/src/styles/typoStyle.ts
@@ -1,66 +1,69 @@
 import { css } from "@emotion/react";
 import { AppTheme } from "./theme";
 
-export const title1Bold = (theme: AppTheme) => css`
-    font-size: ${theme.typo.title1Bold.fontSize};
-    font-weight: ${theme.typo.title1Bold.fontWeight};
-    font-family: ${theme.typo.title1Bold.fontFamily};
-`;
-
-export const title1Regular = (theme: AppTheme) => css`
-    font-size: ${theme.typo.title1Regular.fontSize};
-    font-weight: ${theme.typo.title1Regular.fontWeight};
-    font-family: ${theme.typo.title1Regular.fontFamily};
-`;
-
-export const title2Bold = (theme: AppTheme) => css`
-    font-size: ${theme.typo.title2Bold.fontSize};
-    font-weight: ${theme.typo.title2Bold.fontWeight};
-    font-family: ${theme.typo.title2Bold.fontFamily};
-`;
-
-export const title2Regular = (theme: AppTheme) => css`
-    font-size: ${theme.typo.title2Regular.fontSize};
-    font-weight: ${theme.typo.title2Regular.fontWeight};
-    font-family: ${theme.typo.title2Regular.fontFamily};
-`;
-
-export const body1SemiBold = (theme: AppTheme) => css`
-    font-size: ${theme.typo.body1SemiBold.fontSize};
-    font-weight: ${theme.typo.body1SemiBold.fontWeight};
-`;
-
-export const body1Regular = (theme: AppTheme) => css`
-    font-size: ${theme.typo.body1Regular.fontSize};
-    font-weight: ${theme.typo.body1Regular.fontWeight};
-`;
-
-export const body2SemiBold = (theme: AppTheme) => css`
-    font-size: ${theme.typo.body2SemiBold.fontSize};
-    font-weight: ${theme.typo.body2SemiBold.fontWeight};
-`;
-
-export const body2Regular = (theme: AppTheme) => css`
-    font-size: ${theme.typo.body2Regular.fontSize};
-    font-weight: ${theme.typo.body2Regular.fontWeight};
-`;
-
-export const caption1Regular = (theme: AppTheme) => css`
-    font-size: ${theme.typo.caption1Regular.fontSize};
-    font-weight: ${theme.typo.caption1Regular.fontWeight};
-`;
-
-export const caption1Light = (theme: AppTheme) => css`
-    font-size: ${theme.typo.caption1Light.fontSize};
-    font-weight: ${theme.typo.caption1Light.fontWeight};
-`;
-
-export const caption2Regular = (theme: AppTheme) => css`
-    font-size: ${theme.typo.caption2Regular.fontSize};
-    font-weight: ${theme.typo.caption2Regular.fontWeight};
-`;
-
-export const caption2Light = (theme: AppTheme) => css`
-    font-size: ${theme.typo.caption2Light.fontSize};
-    font-weight: ${theme.typo.caption2Light.fontWeight};
-`;
+export const typoStyle = {
+    title: {
+        bold: (theme: AppTheme) => css`
+            font-size: ${theme.typo.title1Bold.fontSize};
+            font-weight: ${theme.typo.title1Bold.fontWeight};
+            font-family: ${theme.typo.title1Bold.fontFamily};
+        `,
+        regular: (theme: AppTheme) => css`
+            font-size: ${theme.typo.title1Bold.fontSize};
+            font-weight: ${theme.typo.title1Bold.fontWeight};
+            font-family: ${theme.typo.title1Bold.fontFamily};
+        `
+    },
+    subTitle: {
+        bold: (theme: AppTheme) => css`
+            font-size: ${theme.typo.title2Bold.fontSize};
+            font-weight: ${theme.typo.title2Bold.fontWeight};
+            font-family: ${theme.typo.title2Bold.fontFamily};
+        `,
+        regular: (theme: AppTheme) => css`
+            font-size: ${theme.typo.title2Regular.fontSize};
+            font-weight: ${theme.typo.title2Regular.fontWeight};
+            font-family: ${theme.typo.title2Regular.fontFamily};
+        `
+    },
+    body: {
+        semiBold: (theme: AppTheme) => css`
+            font-size: ${theme.typo.body1SemiBold.fontSize};
+            font-weight: ${theme.typo.body1SemiBold.fontWeight};
+        `,
+        regular: (theme: AppTheme) => css`
+            font-size: ${theme.typo.body1Regular.fontSize};
+            font-weight: ${theme.typo.body1Regular.fontWeight};
+        `
+    },
+    subBody: {
+        semiBold: (theme: AppTheme) => css`
+            font-size: ${theme.typo.body2SemiBold.fontSize};
+            font-weight: ${theme.typo.body2SemiBold.fontWeight};
+        `,
+        regular: (theme: AppTheme) => css`
+            font-size: ${theme.typo.body2Regular.fontSize};
+            font-weight: ${theme.typo.body2Regular.fontWeight};
+        `
+    },
+    caption: {
+        regular: (theme: AppTheme) => css`
+            font-size: ${theme.typo.caption1Regular.fontSize};
+            font-weight: ${theme.typo.caption1Regular.fontWeight};
+        `,
+        light: (theme: AppTheme) => css`
+            font-size: ${theme.typo.caption1Light.fontSize};
+            font-weight: ${theme.typo.caption1Light.fontWeight};
+        `
+    },
+    subCaption: {
+        regular: (theme: AppTheme) => css`
+            font-size: ${theme.typo.caption2Regular.fontSize};
+            font-weight: ${theme.typo.caption2Regular.fontWeight};
+        `,
+        light: (theme: AppTheme) => css`
+            font-size: ${theme.typo.caption2Light.fontSize};
+            font-weight: ${theme.typo.caption2Light.fontWeight};
+        `
+    }
+}
