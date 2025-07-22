1e92475c5c92b8ec3bac0646a88a56df6cbd5640
Kimhyerim <ax1021@naver.com>
2025-07-22 17:29:51 +0900

    [Design] 타이틀 추가 / Home 디자인 완료

--- front/src/pages/Home.tsx
+++ front/src/pages/Home.tsx
@@ -1,12 +1,8 @@
 import React from 'react'
 import styled from '@emotion/styled'
 import { typoStyle } from 'styles/typoStyle';
-import bg from '../assets/images/background02.jpg';
+import bg from '../assets/images/background03.jpg';
 import { SearchInput } from 'components/molecules';
-
-const TSt01 = styled.p`
-  ${({theme}) => typoStyle.subTitle.bold(theme)}
-`;
 
 const Container = styled.div`
   width: 100%;
@@ -22,10 +18,45 @@ const Container = styled.div`
   flex-direction: column;
 `;
 
+const Content = styled.div`
+  display: flex;
+  width: 100%;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 20px;
+  margin-bottom: 50px;
+`;
+
+const Title = styled.div`
+  width: 100%;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex-direction: column;
+  gap: 10px;
+`;
+
+const MainText = styled.p`
+  ${({theme}) => typoStyle.title.bold(theme)};
+  color: white;
+`;
+
+const SubText = styled.p`
+  ${({theme}) => typoStyle.subBody.regular(theme)};
+  color: white;
+`;
+
 const Home = () => {
   return (
     <Container>
-      <SearchInput width='50%' height='60px' label='기업명을 입력해주세요.' />
+      <Content>
+        <Title>
+          <SubText>투자에는 역시 요즘기업 보고서!</SubText>
+          <MainText>요즘 제일 잘 나가는 기업은 어디?</MainText>
+        </Title>
+        <SearchInput width='50%' height='60px' label='기업명을 입력해주세요.' />
+      </Content>
     </Container>
   )
 }
