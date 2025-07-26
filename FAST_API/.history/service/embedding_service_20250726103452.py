44da603f251c59493f3ce488be4872d377e0b536
copy-del <dlxlrkxdmssj@gmail.com>
2025-07-26 14:36:07 +0900

    fastAPI 모듈 분리 : DART 데이터 수집, 임베딩 모델활용 추출

---
+++ FAST_API/.history/service/embedding_service_20250726103452.py
@@ -0,0 +1,27 @@
+
+
+from sentence_transformers import SentenceTransformer, util
+from utils.config import SIM_THRESHOLD
+
+model = SentenceTransformer("snunlp/KR-SBERT-V40K-klueNLI-augSTS")
+
+def get_best_matches(target_cols, candidate_cols):
+    if not target_cols or not candidate_cols:
+        return {}
+
+    embeddings1 = model.encode(target_cols, convert_to_tensor=True)
+    embeddings2 = model.encode(candidate_cols, convert_to_tensor=True)
+
+    similarity = util.pytorch_cos_sim(embeddings1, embeddings2)
+    result = {}
+
+    for i, target in enumerate(target_cols):
+        best_idx = similarity[i].argmax().item()
+        score = similarity[i][best_idx].item()
+        match = candidate_cols[best_idx] if score >= SIM_THRESHOLD else None
+        result[target] = {
+            "match": match,
+            "score": round(score, 4)
+        }
+
+    return result
