1ffa5396110a96ae5bba97c996a530285ea8cdb7
gyu8 <dnflalf1029@naver.com>
2025-07-30 19:29:22 +0900

    feat: signup swagger test 완료

--- backend/back/src/main/java/com/project/web/dto/UserDTO.java
+++ backend/back/src/main/java/com/project/web/dto/UserDTO.java
@@ -1,15 +1,20 @@
 package com.project.web.dto;
 
-import com.project.web.vo.UserVO;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.NoArgsConstructor;
 
 @Data
 @Builder
+@NoArgsConstructor
+@AllArgsConstructor
 public class UserDTO {
 	
-	 private UserVO uservo;
-	 private boolean success;
+	
+    private boolean success;
+    private String message;
+    private String userId;
 	 
 }
--- backend/back/src/main/java/com/project/web/mapper/UserMapper.java
+++ backend/back/src/main/java/com/project/web/mapper/UserMapper.java
@@ -2,12 +2,13 @@ package com.project.web.mapper;
 
 import org.apache.ibatis.annotations.Mapper;
 
-import com.project.web.dto.UserDTO;
 import com.project.web.vo.UserVO;
 
 @Mapper
 public interface UserMapper {
-	int signup(UserDTO user);     // 회원가입 (DB저장)
+	
+	int signup(UserVO request);     // 회원가입 (DB저장)
+	
 	UserVO check_id(String userId);   // 아이디 중복 확인
 	
 	
