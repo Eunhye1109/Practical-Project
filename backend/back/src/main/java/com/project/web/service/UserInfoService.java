61f09556b8a6382108a5a9268e18d351de091f7a
gyu8 <dnflalf1029@naver.com>
2025-07-30 21:10:32 +0900

    feat: secession mybatis 미완성. swagger test 필요함.

---
+++ backend/back/src/main/java/com/project/web/service/UserInfoService.java
@@ -0,0 +1,10 @@
+package com.project.web.service;
+
+import com.project.web.dto.UserDTO;
+import com.project.web.vo.UserVO;
+
+public interface UserInfoService {
+
+	UserDTO secession(UserVO request);
+
+}
