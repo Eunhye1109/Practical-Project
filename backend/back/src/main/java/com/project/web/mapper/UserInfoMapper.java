afdd6f600949e38e9bc4220a5f260116b4639567
gyu8 <dnflalf1029@naver.com>
2025-07-31 11:12:15 +0900

    feat: 회원탈퇴 secession swagger test 완료

--- backend/back/src/main/java/com/project/web/mapper/UserInfoMapper.java
+++ backend/back/src/main/java/com/project/web/mapper/UserInfoMapper.java
@@ -4,6 +4,8 @@ import com.project.web.vo.UserVO;
 
 public interface UserInfoMapper {
 
-	int secession(UserVO request);
+	int secession(String userId, String userPw);
+
+	int secession(UserVO vo);
 
 }
