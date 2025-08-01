71d982dfaace48c83dc1184f258747ecbb0f544b
gyu8 <dnflalf1029@naver.com>
2025-08-01 19:18:53 +0900

    관심기업 등록 및 조회 삭제 기능 구현

---
+++ backend/back/src/main/java/com/project/web/dto/FcDTO.java
@@ -0,0 +1,24 @@
+package com.project.web.dto;
+
+import java.util.List;
+
+import com.project.web.vo.FcVO;
+
+import lombok.Builder;
+import lombok.Data;
+import lombok.Singular;
+
+@Data
+@Builder
+public class FcDTO {
+
+    private String userId;
+    private String corpName;
+     // ✅ VO와 이름 일치
+
+    @Singular("fc")            // ✅ 리스트 빌더 안정성 강화
+    private List<FcVO> fcVOList;
+
+    private Boolean success;
+    private String message;
+}
---
+++ backend/back/src/main/java/com/project/web/vo/FcVO.java
@@ -0,0 +1,15 @@
+package com.project.web.vo;
+
+import java.time.LocalDateTime;
+
+import lombok.Data;
+
+
+@Data
+public class FcVO {
+	  	private Long fc_idx; 
+	    private String userId;
+	    private String corpName;
+	    private LocalDateTime createdAt;
+	   
+}
